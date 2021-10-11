28215744e912c2c6cc1170d7e010562d4fde5b6a
mnasrulk <m.nasrul.k@gmail.com>
2021-10-11 14:08:05 +0700

    commit on 'master'

---
+++ ContohArray.java
@@ -0,0 +1,9 @@
+public class ContohArray {
+    public static void main(String[] args) {
+        String[] teman = { "Deni", "Dodi", "Dewi", "Desi" };
+
+        for (int i = 0; i < teman.length; i++) {
+            System.out.println("Indeks ke-" + i + ": " + teman[i]);
+        }
+    }
+}
